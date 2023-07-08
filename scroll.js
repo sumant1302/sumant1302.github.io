9de03c4cb8b793e966ef19d60179eee81313567a
Sumant Kumar <sumant130201@gmail.com>
2023-07-09 03:36:18 +0530

    Adding horizontal scroll feature and adding learni

---
+++ scroll.js
@@ -0,0 +1,27 @@
+//Typing content in the hoem-main section
+var typed = new Typed("#element", {
+	strings: ["<i>Web</i> Developer", "<i>Android</i> Developer", "&#38; <i>Java</i> Developer"],
+    typeSpeed: 40,
+    loop: true,
+});
+//Scrolling through the mouse horizontally in certificate section
+const scrollContainer1 = document.querySelector(".container-certificate");
+
+scrollContainer1.addEventListener("wheel", (evt) => {
+	evt.preventDefault();
+	scrollContainer1.scrollLeft += evt.deltaY;
+});
+const scrollContainer2 = document.querySelector(".container-projects");
+
+scrollContainer2.addEventListener("wheel", (evt) => {
+	evt.preventDefault();
+	scrollContainer2.scrollLeft += evt.deltaY;
+});
+
+const scrollContainer3 = document.querySelector(".work-exp");
+
+scrollContainer3.addEventListener("wheel", (evt) => {
+	evt.preventDefault();
+	scrollContainer3.scrollLeft += evt.deltaY;
+});
+
